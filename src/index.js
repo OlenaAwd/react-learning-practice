c4ed829912161d4669376d1d6c54b038fe82d7b6
OlenaAwd <elena.avdiunina@gmail.com>
2021-10-17 13:52:56 +0300

    emotion use

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {ThemeProvider } from '@emotion/react'
 import './index.css';
-import { App } from './components/App.jsx';
+import { App } from './components/App/App.jsx';
+import {theme} from './constants/theme';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
